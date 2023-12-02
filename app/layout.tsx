b2f807c2a57b9c72fc53d500fface128ab202412
Alohadancemeow <nawongsak@gmail.com>
2023-12-02 15:49:28 +0700

    🍀 create server sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-// import { ModalProvider } from "@/components/providers/modal-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 // import { SocketProvider } from "@/components/providers/socket-provider";
 // import { QueryProvider } from "@/components/providers/query-provider";
 
@@ -31,10 +31,10 @@ export default function RootLayout({
             enableSystem
             storageKey="discord-theme"
           >
-            {/* <SocketProvider>
-              <ModalProvider />
-              <QueryProvider>{children}</QueryProvider>
-            </SocketProvider> */}
+            {/* <SocketProvider> */}
+            <ModalProvider />
+            {/* <QueryProvider>{children}</QueryProvider> */}
+            {/* </SocketProvider> */}
 
             {children}
           </ThemeProvider>
